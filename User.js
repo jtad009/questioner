e3c2434847933478d01ffc8795500b36f1e2fbe2
jtad009 <jtad009@gmail.com>
2019-01-04 23:51:23 +0100

    User entity specification

---
+++ User.js
@@ -0,0 +1,19 @@
+var user = [
+	{
+		"id": 1,
+		"firstname":"Cynthia",
+		"lastname":"Edet",
+		"othername":"Doreen",
+		"email":"israeledet@yahoo.com",
+		"phoneNumber":"09072745678",
+		"username":"epic",
+		"registered":"2018-09-12",
+		"isAdmin": true
+	},
+];
+function login(){
+
+}
+function add(data){
+	user.push(data);
+}
