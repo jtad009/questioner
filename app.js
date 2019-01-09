73bf227d19577258aea53ca48c7ea91551a42126
Israel Edet <jtad009@gmail.com>
2019-01-09 09:41:58 +0100

    route handler version 1

--- app.js
+++ app.js
@@ -1,102 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var questions = require('./lib/questions');
-var meetups = require('./lib/meetups');
-var app = express();
-
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const questionsRoutes = require('./lib/routes/questions');
+const meetupsRoutes = require('./lib/routes/meetups');
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//log every request using our middleware function before proceeding to the express middleware
 /*
  *@param request
  *@param response
  *@param next action to take
  */
-app.use(function(request,response,next){
-	//console.log(` ${request.method} request for '${request.url}' - ${JSON.stringify(request.body)}`);
-	next();//goto next middleware
-
+app.use((_req, res, next) => {
+  next();
 });
-
-//create file server folder
 app.use(express.static('./public'));
 app.use(cors());
-/**
- *add a meetup
- */
-app.post('/api/v1/meetups/',function(request,response){
-	response.json(meetups.create(request.body));
-});
-/**
- *Delete a meetup
- *
- **/
-app.delete('/api/v1/meetups/:id',function(request,response){
-	console.log(request.params);
-	meetups = meetups.filter(function(meetup_list){
-		console.log('New Meetups: '+meetup_list.id+' ID to delete: '+request.params.id);
-		console.log(parseInt(meetup_list.id) !== request.params.id);
-		return parseInt(meetup_list.id) != request.params.id;
-	});
-	response.json(meetups);
-});
-/*
- *get meetup by id
- */
-app.get('/api/v1/meetups/:id',function(request,response){
-	response.json(meetups.view(false,request.params.id));
-});
-/**
- *List all meetups
- */
-app.get('/api/v1/meetups/',function(request,response){
-	var data = meetups.view(true);
+/* CORS HEADER SETTING*/
 
-	response.json(data);
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers',
+    'Origin, X-Rquested-With, Content-Type, Accept,Authorization');
+  if (request.method === 'OPTIONS') {
+    response.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+    return response.status(200).json({});
+  }
+  next();
 });
+app.use('/api/v1/meetups', meetupsRoutes);
+app.use('/api/v1/questions', questionsRoutes);
 
-/**
- *List all meetups upcoming
- */
-app.get('/api/v1/meetups/upcoming/',function(request,response){
+module.exports = app;
 
-});
-/**
- *List all meetups questions
- */
-app.get('/api/v1/questions/',function(request,response){
-	response.json(questions.view(true,0));
-});
-/**
- *post meetups questions
- */
-app.post('/api/v1/questions/',function(request,response){
-	response.json(questions.add(request.body));
-});
-/**
- *Vote meetup questions
- */
-app.patch('/api/v1/questions/:id/upvote',function(request,response){
-	response.json(questions.vote(request.params.id));
-});
-/**
- *Downvote meetup questions
- */
-app.patch('/api/v1/questions/:id/downvote',function(request,response){
-	response.json(questions.down_vote(request.params.id));
-});
-
-/**
- *	RSVP meetup
- */
-app.post('/api/v1/meetups/:id/rsvps',function(request,response){
-	//rsvps.push(request.body);
-	response.json(meetups.rsvp(request.params.id,request.body));
-});
-//listen on port 5000
-app.listen(process.env.PORT || 5000);
-
-console.log("Express app running on port 5000");
-module.exports = app; //this allow for this files inclusion in other modules
