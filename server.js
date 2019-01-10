268d23a09319c71f5a37367ec06de49a28062130
jtad009 <jtad009@gmail.com>
2019-01-10 16:27:05 +0100

    [feature #163081906] included 2 character spacing on all files and resolved all confilicts
    [feature #163081906] included 2 character spacing on all files and resolved all confilicts
    
    [feature #163081906] included 2 character spacing on all files and resolved all confilicts
    
    [feature #163081906] included 2 character spacing on all files and resolved all confilicts

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 4000;
 const server = http.createServer(app);
-server.listen(port,() => {
+server.listen(port, () => {
     return console.log('Express app running on port 4000');
 });
