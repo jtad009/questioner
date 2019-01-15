6e7bd07ccd8e3189e99ceca595a4431d6f70e3b7
jtad009 <jtad009@gmail.com>
2019-01-15 11:09:03 +0100

    [update done to this commit]

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 4000;
+import http from 'http';
+import app from './app';
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port, () => {
     return console.log('Express app running on port 4000');
